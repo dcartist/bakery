93ab8b087960fd745114cb6fd267b64e0fa3f9b4
DCartist <pbannerman13@gmail.com>
2021-02-27 17:54:59 -0500

    adding text and form input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Logo from "./Assets/LOGO.png"
 function App() {
   return (
-    <div className="background" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/image.png'})` }}>
-      
+    <div className="appBase">
+    <div className="introBackground" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/image.png'})` }}>
+      <img src={Logo}/>
+      <h3>JOIN THE TEAM</h3>
+      <form>
+        <p>
+        <input type="text" placeholder="Email Address"></input>
+        </p>
+        <button> SIGN UP</button>
+      </form>
+      <button>Login</button>
+    </div>
+    <div>
+      Another one
+    </div>
     </div>
   );
 }
