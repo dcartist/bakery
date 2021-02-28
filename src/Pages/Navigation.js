f8405f9ff4e809d8e0cc4377222ed97a4270d586
DCartist <pbannerman13@gmail.com>
2021-02-28 15:39:07 -0500

    adding navigation

--- src/Pages/Navigation.js
+++ src/Pages/Navigation.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Logo from "../Assets/LOGO.png"
-
+import {Link} from "react-router-dom"
 export default function Navigation(props) {
     return (
         <div className="Navigation">
-            Navigation
-
-      <img src={Logo} alt="Amelia's Bakery Logo"/>
+        <img src={Logo} alt="Amelia's Bakery Logo"/>
+        <ul>
+            <li><Link to="/">Intro</Link></li>
+            <li><Link to="/main">Main</Link></li>
+        </ul>
         </div>
     )
 }
