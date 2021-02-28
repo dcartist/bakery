0b185de6f94e6de007067b0c54a67b2991ad5163
DCartist <pbannerman13@gmail.com>
2021-02-28 18:27:27 -0500

    fixed Filter Data

--- src/Pages/DishDisplay.js
+++ src/Pages/DishDisplay.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 export default function DishDisplay() {
     const [data, setdata] = useState([])
+    const [dataResults, setReults] = useState([])
+    const [searched, setSearch] = useState('')
     	useEffect(() => {
             axios
             .get(`${process.env.REACT_APP_API_CAT}`)
@@ -11,10 +13,52 @@ export default function DishDisplay() {
             })
             .catch((err) => console.log(err));
 	 }, []);
-    return (
-        <div className="dishesDisplay">
-            {data.filter(info => !info.idMeal.includes("52891")).map((item, index)=>(<div key={index}><h3>{item.strMeal}</h3> <img src={item.strMealThumb} alt={item.strMeal}></img></div>))}
-            
-        </div>
-    )
+     function search(e){
+         e.preventDefault()
+         setReults(data.filter(info => !info.idMeal.includes("52891")).filter(searchResults => searchResults.strMeal.toLowerCase().match(e.target.value.toLowerCase())))
+        console.log(dataResults.length)
+     }
+
+     if (search.length == 0){
+        return (
+            <section>
+                <div>
+                <input type="text" onChange={search}></input>
+                {searched}
+                </div>
+                <div className="dishesDisplay">
+                {data.filter(info => !info.idMeal.includes("52891")).map((item, index)=>(<div key={index}><h3>{item.strMeal}</h3> <img src={item.strMealThumb} alt={item.strMeal}></img></div>))}
+            </div>
+            </section>
+        )
+     } else {
+         if (dataResults.length == 0){
+            return (
+                <section>
+                    <div>
+                    <input type="text" onChange={search}></input>
+                    </div>
+                    <div className="dishesDisplay">
+                    <div> Sorry no Results</div>
+                        
+                   
+                </div>
+                </section>
+            )
+         } else {
+            return (
+                <section>
+                    <div>
+                    <input type="text" onChange={search}></input>
+                    </div>
+                    <div className="dishesDisplay">
+                    {dataResults.map((item, index)=>(<div key={index}><h3>{item.strMeal}</h3> <img src={item.strMealThumb} alt={item.strMeal}></img></div> ))}
+                   
+                </div>
+                </section>
+            )
+         }
+       
+     }
+    
 }
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import DishDisplay from "./DishDisplay"
+import {Route} from "react-router-dom"
 
 export default function Main() {
 
