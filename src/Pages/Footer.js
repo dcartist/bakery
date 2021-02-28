edf74d4674d6e4cac551c9584a32a27e76555c1e
DCartist <pbannerman13@gmail.com>
2021-02-27 21:37:29 -0500

    Adding Footer

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Facebook from "../Assets/FACEBOOK.png"
+import Instagram from "../Assets/INSTA.png"
 
 export default function Footer() {
     return (
-        <div className="Footer">
-            
+        <div className="footer">
+            <div>
+                <img src={Facebook} alt="Facebook"/>
+                <img src={Instagram} alt="Instagram"/>
+            </div>
+            <p>Privacy Policy</p>
         </div>
     )
 }
