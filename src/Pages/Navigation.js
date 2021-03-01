018d72c03a0f366e61ac242da93be4fcdcc125d9
DCartist <pbannerman13@gmail.com>
2021-02-28 19:39:31 -0500

    added cupcakes to results

--- src/Pages/Navigation.js
+++ src/Pages/Navigation.js
@@ -3,12 +3,18 @@ import Logo from "../Assets/LOGO.png"
 import {Link} from "react-router-dom"
 export default function Navigation(props) {
     return (
-        <div className="Navigation">
+        <div>
+        <div className="navigation">
         <img src={Logo} alt="Amelia's Bakery Logo"/>
         <ul>
             <li><Link to="/">Intro</Link></li>
             <li><Link to="/main">Main</Link></li>
         </ul>
         </div>
+        <div className="welcome">
+            Welcome {props.Email.length == 0 ? "You forgot to sign in" : props.Email}
+        </div>
+        </div>
+       
     )
 }
